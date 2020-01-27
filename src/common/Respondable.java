b2acf6c40b65f45c2efafca7fca03e31b6b7ef7f
Derek Shao <shao.derek.now@gmail.com>
2020-01-27 12:56:42 -0500

    feat: Requestable and Respondable

---
+++ src/common/Respondable.java
@@ -0,0 +1,12 @@
+package common;
+
+/**
+ * Sub-classes could be responded to with a message
+ * 
+ * @author Derek Shao
+ *
+ */
+public interface Respondable {
+	
+	public void response(Message msg);
+}
