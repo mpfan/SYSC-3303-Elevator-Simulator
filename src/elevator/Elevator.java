03c635168559ec326dbe1d91f7804da10e68663c
SouheilY <souheil.yaz@gmail.com>
2020-01-29 16:20:23 -0500

    fix: elevator system logic

--- src/elevator/Elevator.java
+++ src/elevator/Elevator.java
@@ -8,15 +8,17 @@ import common.Requestable;
 public class Elevator implements Respondable, Runnable {
 	
 	private Requestable scheduler; 
-	int elevatorNumber;
-	int capacity;
-	int people;
+	private int elevatorNumber;
+	private int capacity;
+	private int people;
 	boolean door;
 	boolean[] buttonPressed;
-	ElevatorSystem eleSys;
+	private ElevatorSystem eleSys;
+	
+	private static final int CAPACITY = 19;
 	
 	public Elevator(int numberOfButtons, int elevatorNumber, ElevatorSystem eleSys, Requestable scheduler) {
-		this.capacity = 0;
+		this.capacity = CAPACITY;
 		this.people = 0;
 		this.door = false;
 		this.buttonPressed = new boolean[numberOfButtons];
@@ -40,6 +42,94 @@ public class Elevator implements Respondable, Runnable {
 	public void response(Message msg) {
 		
 		receivedMessage(msg);
-		this.eleSys.addMessage(msg);
-	}	
+		this.eleSys.request(msg, this);;
+	}
+	
+	
+	/**
+	 * @param capacity set the elevator's capacity
+	 */
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+	
+	/**
+	 * @return the elevator's capacity
+	 */
+	public int getCapacity() {
+		return capacity;
+	}
+	
+	/**
+	 * @param people set the amount of people in the elevator
+	 */
+	public void setPeople(int people) {
+		this.people = people;
+	}
+	
+	/**
+	 * @return amount of people currently in elevator
+	 */
+	public int getPeople() {
+		return people;
+	}
+	
+	/**
+	 * @param door the door to set
+	 */
+	public void setDoor(boolean door) {
+		this.door = door;
+	}
+
+	/**
+	 * @return 
+	 */
+	public boolean getDoor() {
+		return door;
+	}
+
+	/**
+	 * @return the button pressed
+	 */
+	public boolean[] getButtonPressed() {
+		return buttonPressed;
+	}
+
+	/**
+	 * @param buttonPressed the button pressed to set
+	 */
+	public void setButtonPressed(boolean[] buttonPressed) {
+		this.buttonPressed = buttonPressed;
+	}
+
+
+	/**
+	 * @return the elevatorSystem
+	 */
+	public ElevatorSystem getElevatorSystem() {
+		return eleSys;
+	}
+
+	/**
+	 * @param elevatorSystem the elevatorSystem to set
+	 */
+	public void setElevatorSystem(ElevatorSystem eleSys) {
+		this.eleSys = eleSys;
+	}
+
+	/**
+	 * @return the elevatorNumber
+	 */
+	public int getElevatorNumber() {
+		return elevatorNumber;
+	}
+
+	/**
+	 * @param elevatorNumber the elevatorNumber to set
+	 */
+	public void setElevatorNumber(int elevatorNumber) {
+		this.elevatorNumber = elevatorNumber;
+	}
+	
+	
 }
--- src/elevator/ElevatorSystem.java
+++ src/elevator/ElevatorSystem.java
@@ -5,27 +5,31 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
+import javax.naming.spi.Resolver;
+
 import common.Message;
 import common.Requestable;
+import common.Respondable;
 import scheduler.Scheduler;
 
-public class ElevatorSystem implements Runnable {
+public class ElevatorSystem implements Runnable, Respondable, Requestable {
 	
 	private Requestable scheduler; 
 	private List<Elevator> elevators;
-	private Queue<Message> requests;
+	private Queue<Message> inBoundRequests, outBoundRequests;
 	
 	public ElevatorSystem(Scheduler scheduler) {
 		this.scheduler = scheduler;
 		this.elevators = new ArrayList<Elevator>();
-		this.requests = new LinkedList<Message>();
+		this.inBoundRequests = new LinkedList<Message>();
+		this.outBoundRequests = new LinkedList<Message>();
 		this.addElevator();
 		startSystem();
 	}
 	
 	public void addElevator() {
 		this.elevators.add(new Elevator(10, 0, this, scheduler));
-		this.elevators.add(new Elevator(10, 1, this, scheduler));
+//		this.elevators.add(new Elevator(10, 1, this, scheduler));
 	}
 	
 	public void startSystem() {
@@ -35,24 +39,35 @@ public class ElevatorSystem implements Runnable {
 		}
 	}
 	
-	public synchronized void addMessage(Message msg) {
-		synchronized(requests) {
-			this.requests.add(msg);
-		}
-	} 
-
 	@Override
 	public void run() {
-		
-		synchronized(requests) {
-			while (this.requests.isEmpty()) {
+		synchronized(inBoundRequests) {
+			while (this.inBoundRequests.isEmpty()) {
 				try {
-					wait();
+//					wait();
+					this.scheduler.request(null, this);
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
+//			this.scheduler.request(inBoundRequests.poll(), this);
 		}
 	}
+
+	@Override
+	public void response(Message msg) {
+		this.inBoundRequests.add(msg);
+	}
+
+	@Override
+	public void request(Message msg, Respondable obj) {
+		synchronized(outBoundRequests) {
+			this.scheduler.request(outBoundRequests.poll(), this);
+		}
+	}
+	
+
+	
+	
 }
