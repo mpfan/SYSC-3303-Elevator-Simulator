7d928df6f1a4dfca130552b76973fe22695ccacc
Hoang Bui <hoangxuanbui6572@gmail.com>
2020-01-30 20:52:40 -0500

    doc: Added comments to the floor system

--- src/floor/FloorDoor.java
+++ src/floor/FloorDoor.java
@@ -1,17 +1,36 @@
 package floor;
 
+/**
+* A class to represent the floor doors that opens for elevators
+*
+* @author Hoang Bui, Christoph Tran
+*/
 public class FloorDoor {
+	// Variable
 	boolean isOpen;
 	
+	/**
+	* Constructor for a floor door
+	*/
 	public FloorDoor() {
 		this.isOpen = false;
 	}
 	
+	/**
+	* Method to open the door
+	*
+	* @return true when the door has opened
+	*/
 	public boolean openDoor() {
 		isOpen = true;
 		return isOpen;
 	}
 	
+	/**
+	* Method to close the door
+	*
+	* @return false when the door has closed
+	*/
 	public boolean closeDoor() {
 		isOpen = false;
 		return isOpen;
