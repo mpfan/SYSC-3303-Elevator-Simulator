e9080b35742a2bd8e1722b2bebe098b4aa222e34
Souheil-Yazji <35379392+Souheil-Yazji@users.noreply.github.com>
2020-02-15 10:18:02 -0500

    feat: added send message on destination reached

--- src/elevator/Elevator.java
+++ src/elevator/Elevator.java
@@ -1,11 +1,16 @@
 package elevator;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Calendar;
 
+
+import common.FloorMessage;
 import common.Message;
 import common.MessageType;
+import common.ElevatorState.Transition;
 
 /**
  * 
@@ -16,16 +21,18 @@ import common.MessageType;
  */
 public class Elevator implements Runnable {
 
-	//Variables
+	// Variables
 	private int elevatorNumber; // elevator identifier
 	private int capacity;
 	private int people;
+	private int currFloor; //current floor
+	private int currDest; //current destination
 	private boolean door;
 	private boolean[] buttonPressed;
 	private ElevatorSystem eleSys;
-	private Message msg;
+	private Message msg, eleMsg;
 
-	private HashSet<Integer> destination;
+	private HashSet<Integer> destinations;
 
 	private ElevatorStateMachine state;
 	private ElevatorMode mode; // indicates if the elevator is meant to go up or down
@@ -35,9 +42,12 @@ public class Elevator implements Runnable {
 	/**
 	 * Constructor for elevator
 	 * 
-	 * @param numberOfButtons number of buttons in the elevator
-	 * @param elevatorNumber the elevator number
-	 * @param eleSys the elevator system
+	 * @param numberOfButtons
+	 *            number of buttons in the elevator
+	 * @param elevatorNumber
+	 *            the elevator number
+	 * @param eleSys
+	 *            the elevator system
 	 */
 	public Elevator(int numberOfButtons, int elevatorNumber, ElevatorSystem eleSys) {
 		this.capacity = CAPACITY;
@@ -47,7 +57,8 @@ public class Elevator implements Runnable {
 		this.eleSys = eleSys;
 		this.elevatorNumber = elevatorNumber;
 		this.state = new ElevatorStateMachine();
-		this.destination = new HashSet<Integer>();
+		this.destinations = new HashSet<Integer>();
+		this.currFloor = 0;
 	}
 
 	/**
@@ -60,9 +71,9 @@ public class Elevator implements Runnable {
 			processMessage();
 		}
 	}
-	
+
 	/**
-	 *  Method to process the message sent by elevator system
+	 * Method to process the message sent by elevator system
 	 */
 	public synchronized void processMessage() {
 		while (msg == null) {
@@ -73,17 +84,36 @@ public class Elevator implements Runnable {
 			}
 		}
 		System.out.println("Elevator: Processing message in elevator...");
-		System.out.println("Elevator: " + msg.getBody());
+		loadFloorMessage(msg);
+		System.out.println("Elevator " + elevatorNumber +  ": " + msg.getBody());
+		
+		while(currFloor != currDest) {
+			if(currFloor < currDest) {
+				state.onNext(Transition.RECEIVEDMESSAGE_UP);
+				currFloor++;
+			} else if (currFloor > currDest) {
+				state.onNext(Transition.RECEIVEDMESSAGE_DOWN);
+				currFloor--;
+			} else {
+				state.onNext(Transition.REACHEDDESTINATION);
+			}
+			createEleMsg(); // create new elevator message
+			this.eleSys.addOutboundMessage(eleMsg);	// send to system to send to scheduler		
+		}
+		
+		
 		msg.setType(MessageType.ELEVATOR);
 		this.eleSys.addOutboundMessage(msg);
+		
 		this.msg = null;
 		notifyAll();
 	}
-	
+
 	/**
 	 * Method for elevator to do work specified by message
 	 * 
-	 * @param msg Message with work for elevator to do
+	 * @param msg
+	 *            Message with work for elevator to do
 	 */
 	public synchronized void request(Message msg) {
 
@@ -98,21 +128,22 @@ public class Elevator implements Runnable {
 		this.msg = msg;
 		notifyAll();
 	}
-	
+
 	/**
 	 * Get the message currently stored in elevator
 	 * 
 	 * @return messaged stored in elevator
 	 */
 	public Message getMessage() {
-		
+
 		return msg;
 	}
 
 	/**
 	 * Set the elevator's capacity of the number of people
 	 * 
-	 * @param capacity The capacity of the elevator
+	 * @param capacity
+	 *            The capacity of the elevator
 	 */
 	public void setCapacity(int capacity) {
 		this.capacity = capacity;
@@ -128,13 +159,21 @@ public class Elevator implements Runnable {
 	}
 
 	/**
+	 * @return the currFloor
+	 */
+	public int getCurrFloor() {
+		return currFloor;
+	}
+
+	/**
 	 * Set the amount of people in the elevator
 	 * 
-	 * @param people The amount of people in the elevator
+	 * @param people
+	 *            The amount of people in the elevator
 	 * @return true if the number of people has been changed, false otherwise
 	 */
 	public boolean setPeople(int people) {
-		if(people > capacity) {
+		if (people > capacity) {
 			return false;
 		}
 		this.people = people;
@@ -162,7 +201,8 @@ public class Elevator implements Runnable {
 	/**
 	 * Set the elevator system instance
 	 * 
-	 * @param elevatorSystem the elevatorSystem to set
+	 * @param elevatorSystem
+	 *            the elevatorSystem to set
 	 */
 	public void setElevatorSystem(ElevatorSystem eleSys) {
 		this.eleSys = eleSys;
@@ -180,7 +220,8 @@ public class Elevator implements Runnable {
 	/**
 	 * Sets the elevator number
 	 * 
-	 * @param elevatorNumber the elevatorNumber to set
+	 * @param elevatorNumber
+	 *            the elevatorNumber to set
 	 */
 	public void setElevatorNumber(int elevatorNumber) {
 		this.elevatorNumber = elevatorNumber;
@@ -188,20 +229,50 @@ public class Elevator implements Runnable {
 
 	/**
 	 * Add destination to elevator's Set of destination floors
+	 * 
 	 * @param destination
 	 */
 	public void addDestination(Integer destination) {
-		this.destination.add(destination);
+		this.destinations.add(destination);
 	}
-	
-	
+
 	/**
 	 * Mode indicating if the elevator is meant to go up or down
 	 *
 	 */
 	public enum ElevatorMode {
-		UP,
-		DOWN
+		UP, DOWN
 	}
+	
+	
+	public void loadFloorMessage(Message message) {
+		FloorMessage msg = new FloorMessage(message);
+		Integer newDest = new Integer(msg.getFloorNum());
+		
+//		if (!destinations.isEmpty()) {
+//			if (mode == ElevatorMode.DOWN && newDest > currDest) {
+//				currDest = newDest;
+//			} 
+//		} else {
+//			currDest = newDest;
+//		}
+		currDest = newDest;
+		addDestination(newDest);
+	}
+	
+	
+	
+	private Message createEleMsg() {
+		Calendar cal = Calendar.getInstance();
+		int hh = cal.get(Calendar.HOUR_OF_DAY);
+		int mm = cal.get(Calendar.MINUTE);
+		int ss = cal.get(Calendar.SECOND);
+		int ms = cal.get(Calendar.MILLISECOND);
+		
+		String body = hh + ":" + mm + ":" + ss + ":" + ms + "," + currFloor + "," + state + "," + currDest + "," + elevatorNumber;
 
+		eleMsg = new Message(MessageType.ELEVATOR, body);
+		return eleMsg;
+	}
+	
 }
