b6d283bc510370111b5feb69683831bd84aafd3b
Michael <mfan20107@gmail.com>
2020-02-15 22:03:54 -0500

    doc: fix typo in MessengerTest.java

--- src/common/MessengerTest.java
+++ src/common/MessengerTest.java
@@ -8,7 +8,7 @@ import java.net.UnknownHostException;
 import org.junit.jupiter.api.Test;
 
 /**
- * test class for CLasee Messenger
+ * Test class for Messenger
  * 
  * @author Michael Fan, Hoang Bui
  */
